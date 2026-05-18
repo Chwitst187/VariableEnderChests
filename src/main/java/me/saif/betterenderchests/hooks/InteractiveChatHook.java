e96e1d2e2cc81f9c2caf1d519259c35216433ff1
Chris <90093334+Chwitst187@users.noreply.github.com>
2026-05-18 19:22:29 +0200

    Remove hard dependency on InteractiveChat artifact

--- src/main/java/me/saif/betterenderchests/hooks/InteractiveChatHook.java
+++ src/main/java/me/saif/betterenderchests/hooks/InteractiveChatHook.java
@@ -1,36 +1,86 @@
 package me.saif.betterenderchests.hooks;
 
-import com.loohp.interactivechat.api.events.InventoryPlaceholderEvent;
 import me.saif.betterenderchests.VariableEnderChests;
 import me.saif.betterenderchests.enderchest.EnderChest;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventException;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.EventExecutor;
+import org.bukkit.plugin.Plugin;
+
+import java.lang.reflect.Method;
 
 public class InteractiveChatHook implements Listener {
 
-    private VariableEnderChests plugin;
+    private static final String EVENT_CLASS_NAME = "com.loohp.interactivechat.api.events.InventoryPlaceholderEvent";
+    private static final String ENDERCHEST_PLACEHOLDER_TYPE_NAME = "ENDERCHEST";
+
+    private final VariableEnderChests plugin;
 
     public InteractiveChatHook(VariableEnderChests plugin) {
-        if (Bukkit.getPluginManager().getPlugin("InteractiveChat") == null) {
+        this.plugin = plugin;
+
+        Plugin interactiveChat = Bukkit.getPluginManager().getPlugin("InteractiveChat");
+        if (interactiveChat == null) {
             return;
         }
-        plugin.getLogger().info("Hooked into InteractiveChat");
-        Bukkit.getPluginManager().registerEvents(this, plugin);
-        this.plugin = plugin;
+
+        try {
+            Class<?> eventClass = Class.forName(EVENT_CLASS_NAME, true, interactiveChat.getClass().getClassLoader());
+            if (!Event.class.isAssignableFrom(eventClass)) {
+                plugin.getLogger().warning("InteractiveChat hook failed: InventoryPlaceholderEvent is not a Bukkit Event");
+                return;
+            }
+
+            @SuppressWarnings("unchecked")
+            Class<? extends Event> bukkitEventClass = (Class<? extends Event>) eventClass;
+            Bukkit.getPluginManager().registerEvent(
+                    bukkitEventClass,
+                    this,
+                    EventPriority.NORMAL,
+                    new InventoryPlaceholderEventExecutor(plugin),
+                    plugin
+            );
+            plugin.getLogger().info("Hooked into InteractiveChat");
+        } catch (ClassNotFoundException ignored) {
+            plugin.getLogger().warning("InteractiveChat hook failed: InventoryPlaceholderEvent class was not found");
+        }
     }
 
-    @EventHandler
-    private void onInventoryDisplay(InventoryPlaceholderEvent event) {
-        if (event.getType() != InventoryPlaceholderEvent.InventoryPlaceholderType.ENDERCHEST)
-            return;
+    private static class InventoryPlaceholderEventExecutor implements EventExecutor {
 
-        Player player = event.getSender().getLocalPlayer();
-        plugin.getEnderChestManager().updateRows(player);
-        EnderChest enderChest = plugin.getEnderChestManager().getEnderChest(player);
+        private final VariableEnderChests plugin;
 
-        event.setInventory(enderChest.getInventory());
+        private InventoryPlaceholderEventExecutor(VariableEnderChests plugin) {
+            this.plugin = plugin;
+        }
+
+        @Override
+        public void execute(Listener listener, Event event) throws EventException {
+            try {
+                Object placeholderType = event.getClass().getMethod("getType").invoke(event);
+                if (!ENDERCHEST_PLACEHOLDER_TYPE_NAME.equals(String.valueOf(placeholderType))) {
+                    return;
+                }
+
+                Object sender = event.getClass().getMethod("getSender").invoke(event);
+                Object localPlayer = sender.getClass().getMethod("getLocalPlayer").invoke(sender);
+                if (!(localPlayer instanceof Player)) {
+                    return;
+                }
+
+                Player player = (Player) localPlayer;
+                plugin.getEnderChestManager().updateRows(player);
+                EnderChest enderChest = plugin.getEnderChestManager().getEnderChest(player);
+
+                Method setInventoryMethod = event.getClass().getMethod("setInventory", org.bukkit.inventory.Inventory.class);
+                setInventoryMethod.invoke(event, enderChest.getInventory());
+            } catch (ReflectiveOperationException e) {
+                throw new EventException(e);
+            }
+        }
     }
-
 }
